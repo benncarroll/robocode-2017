49e99028cf0d24990c50b524fa3b440714e2e342
Ben Carroll <ben.carroll@student.tsc.nsw.edu.au>
2017-11-01 23:33:50 +1100

    Large update - Mainly explanatory stuff
    Added:
    - Huge amount of comments
    - System to go searching the map if we dont see any bots
    - Lots more data to graphical debugging, now shows predicted pos and
    works
    - Example simulations and way more data to README
    
    Modified:
    - Packaged data output into a function
    - Removed hard coded bullet speed in targeting function and replaced it
    with legitimate calculation based on determined bullet power
    - Slight mod to radar lock to keep it wider and not lose track when
    skipping a turn
    - Fixed onHitRobot function as it was actually our downfall, and was
    trying to control the robot from within itself
    - README to be uploaded to forums
    
    Removed:
    - Lots of now dead and unused variables

--- Feral.java
+++ Feral.java
@@ -18,58 +18,68 @@ import java.util.List;
 
 import java.lang.*;
 
+// |-|                          This is Feral.                          |-|
+// |-|            Carroll's AdvancedRobot which makes use of            |-|
+// |-|  predictive targeting, oscillator movement and a locking radar.  |-|
+// |-|                            good luck ;p                          |-|
 
-// API help : http://robocode.sourceforge.net/docs/robocode/robocode/Robot.html
-
-/**
- *   Feral - a robot by Carroll
- */
 
 public class Feral extends AdvancedRobot {
 
-double prevEnergy = 100.0;
-
+// Setup global vars
 boolean rescanning = false;
-long lastScan = 0;
-
-double square_angle = 90;
 byte moveDirection = 1;
-long lastTime = 0;
 
-double scanDir = 1;
-
+double enemyHeadingChange;
 double oldEnemyHeading;
 
+long lastScan = 0;
+long lastTime = 0;
+
+String target;
+String ally = "PHA";
+
+
+// Database
 Map<String, ScannedRobot> botList = new HashMap<String, ScannedRobot>();
 
-/**
- * run: Square's default behavior
- */
 public void run() {
-        // Initialization of the robot should be put here
 
+        // pretty colours :0
         Color realOrange = new Color(255, 100, 0);
-        setColors(realOrange, realOrange, Color.black, realOrange, realOrange); // body,gun,radar, bullet, scan
+        setColors(realOrange, Color.black, Color.black, realOrange, realOrange); // body, gun, radar, bullet, scan
 
+        // Make our gun and radar independent of tank body
         setAdjustGunForRobotTurn(true);
         setAdjustRadarForGunTurn(true);
 
-        setTurnRadarLeftRadians(Double.POSITIVE_INFINITY);
+        setTurnRadarRightRadians(Double.POSITIVE_INFINITY);
 
         // Robot main loop
         while (true) {
 
-                setAhead(0);
+
+                // If radar is done turning back, start it up again
                 if ( getRadarTurnRemaining() == 0.0 ) {
                         setTurnRadarRightRadians( Double.POSITIVE_INFINITY );
                 }
 
+                // If we don't see any bots, start a really wide curve
+                if (botList.size() == 0.0) {
+                        setTurnRight(0.1);
+                        setAhead(500);
+                }
+
+                // Stops the robot from getting disabled
+                setAhead(0);
                 execute();
 
         }
 }
 
 public void smartFire(String bot) {
+
+        target = bot;
 
         ScannedRobot m = ScannedRobot.class.cast(botList.get(bot));
 
@@ -100,29 +110,40 @@ public void smartFire(String bot) {
                 power = power/2;
         }
 
-        // Turn gun to shoot ahead of enemy
 
+        // Super-cool iterative targeting algorithm
+        // Works by finding the rate of change of their heading, and therefore
+        // works as linear, circular and stationary all in one.
         double enemyHeading = m.headingR;
-        double enemyHeadingChange = enemyHeading - oldEnemyHeading;
+        enemyHeadingChange = enemyHeading - oldEnemyHeading;
         oldEnemyHeading = enemyHeading;
         double absBearing = m.bearingR + getHeadingRadians();
         double deltaTime = 0.0;
         double pX = getX() + m.distance * Math.sin(absBearing);
         double pY = getY() + m.distance * Math.cos(absBearing);
-        while (++deltaTime * 11.0D < Point2D.Double.distance(getX(), getY(), pX, pY))
+        while (++deltaTime * (20- power * power) < Point2D.Double.distance(getX(), getY(), pX, pY))
         {
+                // Triggerednometry
                 pX += Math.sin(enemyHeading) * m.velocity;
                 pY += Math.cos(enemyHeading) * m.velocity;
 
                 enemyHeading += enemyHeadingChange;
 
+                // Limit predicted positions to inside the battlefield.
+                // 18 is half the width of a robot, and therefore limits of any
+                // robots X coordinate to between 18 & getBattleFieldWidth - 18,
+                // and vice versa.
+
                 pX = Math.max(Math.min(pX, getBattleFieldWidth() - 18.0D), 18.0D);
                 pY = Math.max(Math.min(pY, getBattleFieldHeight() - 18.0D), 18.0D);
         }
 
+        // Update values in bot data to be used in graphical debug
+        m.pX = pX;
+        m.pY = pY;
+
         double gunTurnAngle = Utils.normalAbsoluteAngle(Math.atan2(pX - getX(), pY - getY()));
         setTurnGunRightRadians(Utils.normalRelativeAngle(gunTurnAngle - getGunHeadingRadians()));
-
 
         fire(power);
 
@@ -134,8 +155,14 @@ public void smartFire(String bot) {
  */
 public void onScannedRobot(ScannedRobotEvent e) {
 
+        if (e.getName().contains(ally) && getOthers() > 1) {
+                return;
+        }
+
         if (!rescanning) {
 
+                // Check when the last rescan was and rescan if it was over 50
+                // turns ago.
                 if (getOthers() > 1 && lastScan < getTime() - 50) {
                         lastScan = getTime();
                         rescanning = true;
@@ -146,7 +173,7 @@ public void onScannedRobot(ScannedRobotEvent e) {
                 // Lock radar
                 double angleToEnemy = getHeadingRadians() + e.getBearingRadians();
                 double radarTurn = Utils.normalRelativeAngle( angleToEnemy - getRadarHeadingRadians() );
-                double extraTurn = Math.min( Math.atan( 60.0 / e.getDistance() ), Rules.RADAR_TURN_RATE_RADIANS );
+                double extraTurn = Math.min( Math.atan( 70.0 / e.getDistance() ), Rules.RADAR_TURN_RATE_RADIANS );
                 if (radarTurn < 0)
                         radarTurn -= extraTurn;
                 else
@@ -167,38 +194,20 @@ public void onScannedRobot(ScannedRobotEvent e) {
                 trimBotList();
 
                 // Determine closest bot
-                double closestDist = 10000.0;
-                String closestName = "";
+                double closestDist = Double.MAX_VALUE;
+                String closestName = new String();
 
                 for (Map.Entry enemy: botList.entrySet()) {
                         ScannedRobot m = ScannedRobot.class.cast(enemy.getValue());
-                        //out.println(m.getKey() +" " + m.getValue());
                         if (m.distance <= closestDist) {
                                 closestDist = m.distance;
                                 closestName = m.name;
                         }
                 }
 
-                // square_angle = Math.abs(botList.get(closestName).heading - 90);
-                // turnRight(square_angle);
-
-                for (int i=0; i<11; i++) {
-                        out.println();
-                }
-
-                for (Map.Entry enemy: botList.entrySet()) {
-                        ScannedRobot m = ScannedRobot.class.cast(enemy.getValue());
-                        out.println("--- " + m.name + " ---");
-                        out.println("Dist:      " + m.distance);
-                        out.println("Update:    " + m.lastUpdate + "/" + getTime());
-                        out.println("Stat.:     " + m.stationary);
-                        out.println("           X: " + m.X + " Y: "+ m.Y);
-                }
-
-                out.println();
-                out.println("Tracking:  " + closestName);
-
                 smartFire(closestName);
+
+                outputStream();
         }
 }
 
@@ -240,44 +249,94 @@ public void onRobotDeath(RobotDeathEvent e) {
 
 
 public void onHitRobot(HitRobotEvent e) {
-        moveDirection *= -1;
-        turnRadarRight(e.getBearing());
+
+        // If it was our fault, simply turn around
+        if (e.isMyFault()) {
+                moveDirection *= -1;
+        }
+
+        // Construct a new ScannedRobot class and add to the botList and let
+        // our loop handle that, instead of trying to manually take over and
+        // target it from here.
+        ScannedRobotEvent m = new ScannedRobotEvent(
+                e.getName(),
+                e.getEnergy(),
+                e.getBearing(),
+                36,
+                // Heading and velocity aren't supplied by HitRobotEvent, but we
+                // can assume it is stopped and heading toward us. It doesn't
+                // really matter anyway as our main loop will update these with
+                // correct values.
+                -(e.getBearing()),
+                0,
+                false);
+
+        // Add our bot to our main list
+        botList.put(e.getName(), new ScannedRobot(m));
+
+}
+
+public void outputStream() {
+
+        // Clear some space
+        for (int i=0; i<11; i++) {
+                out.println();
+        }
+
+        // Print out data on every bot in our database
+        for (Map.Entry enemy: botList.entrySet()) {
+                ScannedRobot m = ScannedRobot.class.cast(enemy.getValue());
+                out.println("--- " + m.name + " ---");
+                out.println("Dist:      " + m.distance);
+                out.println("Update:    " + m.lastUpdate + "/" + getTime());
+                out.println("Stat.:     " + m.stationary);
+                out.println("           X: " + m.X + " Y: "+ m.Y);
+        }
+
+        out.println();
+        out.println("Tracking:  " + target);
 
 }
 
 public void onPaint(Graphics2D g) {
+        // Indicator to show us if we are rescanning
         if (rescanning) {
                 g.setColor(Color.blue);
-                int x = (int) (getX() - 100.0);
-                int y = (int) (getY() - 100.0);
+                int x = (int) (getX() - 50.0);
+                int y = (int) (getY() - 50.0);
                 g.drawOval(x,y,100,100);
         } else {
-                for (Map.Entry enemy:botList.entrySet()) {
-                        ScannedRobot e = ScannedRobot.class.cast(enemy.getValue());
 
-                        int x = e.X;
-                        int y = e.Y;
+                ScannedRobot m = ScannedRobot.class.cast(botList.get(target));
 
-                        // double px = e.pX;
-                        // double py = e.pY;
+                // gather coords from bot data
+                int x = m.X;
+                int y = m.Y;
+                int px = (int) m.pX;
+                int py = (int) m.pY;
 
-                        int r;
-                        r = 36;
-                        x = x-(r/2);
-                        y = y-(r/2);
+                int r = 36;
 
-                        // Draw circle on enemies last pos
-                        g.setColor(new java.awt.Color((int) (255 - (int) e.distance/4),(int) (e.distance/3),0));
-                        g.fillOval(x,y,r,r);
+                // c for circle, vars to be used in circle drawings
+                int cx = x-(r/2);
+                int cy = y-(r/2);
+                int cpx = px-(r/2);
+                int cpy = py-(r/2);
 
-                        g.setColor(Color.blue);
-                        g.drawString(Long.toString(e.lastUpdate),x,y + 60);
 
-                        // // Draw green circle on enemies predicted pos
-                        // g.setColor(new java.awt.Color(0,255,0,255));
-                        // g.fillOval((int)x,(int) y,r,r);
+                // Draw circle on enemies last pos
+                g.setColor(new java.awt.Color((int) limit((255 - (int) m.distance/4), 0, 255), (int) limit((m.distance/3), 0, 255), 0));
+                g.fillOval(cx,cy,r,r);
 
-                }
+                // Draw green circle on enemies predicted pos
+                g.setColor(new java.awt.Color(0,255,0,255));
+                g.fillOval((int) cpx,(int) cpy,r,r);
+                g.drawLine(x,y,px,py);
+
+                // Shows the turn this bot's data was last updated.
+                // -- No longer necessary as we cull 'old' bot data
+                // g.setColor(Color.blue);
+                // g.drawString(Long.toString(m.lastUpdate),cx,cy + 60);
         }
 }
 
@@ -292,27 +351,25 @@ public static int randInt(int min, int max) {
 }
 
 public static double randDouble() {
+        // Simplicity? idk
         Random rand = new Random();
         double randomNum = rand.nextDouble();
         return randomNum;
 }
 
-/**
- * onHitWall: What to do when you hit a wall
- */
 public void onHitWall(HitWallEvent e) {
-        // Replace the next line with any behavior you would like
         turnRight(90);
 }
 
-// normalizes a bearing to between +180 and -180
 double normalizeBearing(double angle) {
+        // normalizes a bearing to between +180 and -180 iteratively
         while (angle > 180) angle -= 360;
         while (angle < -180) angle += 360;
         return angle;
 }
 
 public void onWin(WinEvent e) {
+        // Spin EVERYTHING
         setTurnRadarRightRadians(Double.POSITIVE_INFINITY);
         setTurnGunLeftRadians(Double.POSITIVE_INFINITY);
         setTurnRight(1000);
@@ -323,8 +380,8 @@ class ScannedRobot {
 String name;
 int X;
 int Y;
-// double pX;
-// double pY;
+double pX;
+double pY;
 double energy;
 double bearing;
 double bearingR;
@@ -337,6 +394,8 @@ boolean stationary;
 
 
 ScannedRobot(ScannedRobotEvent e) {
+        // Setting the name isn't in updateBot just to make sure
+        // stuff doesnt get corrupted
         name = e.getName();
         updateBot(e);
 }
@@ -350,10 +409,6 @@ void updateBot(ScannedRobotEvent e) {
         heading = e.getHeading();
         headingR = e.getHeadingRadians();
         velocity = e.getVelocity();
-
-        // Point2D.Double ppos = getPredictedPos(e);
-        // pX = ppos.x;
-        // pY = ppos.y;
 
         Point2D.Double pos = getPos(e);
 
@@ -371,10 +426,14 @@ void updateBot(ScannedRobotEvent e) {
 }
 
 void bulletUpdate(BulletHitEvent e) {
+        // Not using StopNGo movement system anymore, not sure if we
+        // still need this.
         energy = e.getEnergy();
 }
 
 Point2D.Double getPos(ScannedRobotEvent event) {
+        // More trig
+
         double distance = event.getDistance();
         double angle = getHeadingRadians() + event.getBearingRadians();
 
@@ -389,6 +448,7 @@ Point2D.Double getPos(ScannedRobotEvent event) {
 }
 
 private double limit(double value, double min, double max) {
+        // very very handy function
         return Math.min(max, Math.max(min, value));
 }
 
--- README.md
+++ README.md
@@ -2,14 +2,29 @@
 
 ### Overview
 
-Feral is a bot built to participate in a class melee competition, and is built of the AdvancedRobot class.
+Feral is a bot built to participate in a class melee competition, and is built off the AdvancedRobot class.
 
 ### Targeting
 
-Feral uses a simple linear targeting algorithm to shoot at enemies predicted positions. To find and gather data about enemies, it locks its radar onto the closest bot to get data as fast as possible. Every 50 turns, it turns the radar 360 degrees to re-calculate the closest bot.
+Feral uses a linear targeting algorithm to shoot at enemies predicted positions. To find and gather data about enemies, it locks its radar onto the closest bot to get data as fast as possible. Every 50 turns, it turns the radar 360 degrees to re-calculate the closest bot.
 
 ### Movement
 
-Feral uses oscillator movement, and keeps itself perpendicular to its angle to the enemy. It chooses a random 
+Feral uses strafing movement, and keeps itself perpendicular to its angle to the enemy. It chooses a random distance between 25% above and below 185px to move after each turn. Its movement is based entirely off the robot it is currently tracking.
 
-### Other Features
+### Cool Features
+
+To store data, Feral uses a map of custom classes which store a lot of data on each bot, including distance, velocity, bearing, coordinates, predicted coordinates and the last time it was scanned.
+
+Feral will automatically disable its rescan feature if there is only one bot left in the arena, to ensure it never misses anything the enemy does.
+
+### See it in action
+
+#### Feral vs. Tracker
+![Feral vs. Tracker](img/FeralvTracker.gif)
+
+#### Feral vs. Crazy
+![Feral vs. Crazy](img/FeralvCrazy.gif)
+
+#### Feral vs. VelociRobot
+![Feral vs. VelociRobot](img/FeralvVelociRobot.gif)
